ea876d3ac52630150121e949e2d67c0b4df795ea
Petrik Diak <diak@petrik.hu>
2025-10-07 11:56:49 +0200

    measurment adding done

--- wtfwpf/MainWindow.xaml.cs
+++ wtfwpf/MainWindow.xaml.cs
@@ -11,14 +11,52 @@ using System.Windows.Shapes;
 
 namespace wtfwpf
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		//mérés lista
+		List<Measurement> measurements = new List<Measurement>();
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void RefreshList()
+		{
+			listBox.Items.Clear();
+
+			//rendezés idő szerint majd kiírás
+			foreach (var m in measurements.OrderBy(m => m.Time))
+			{
+				listBox.Items.Add(m.ToString());
+			}
+		}
+
+		private void btnAdd_Click(object sender, RoutedEventArgs e)
+		{
+			//ellenőrzések
+			if(!double.TryParse(txtTime.Text, out double t) || !double.TryParse(txtSpeed.Text, out double s) || t < 0 || s < 0)
+			{
+				MessageBox.Show("Hibás adat bevitel!");
+				return;
+			}
+
+			//ha már van ilyen időpontú mérés akkor azt ne adjuk hozzá
+			if (measurements.Any(x => x.Time == t))
+			{
+				MessageBox.Show("Ilyen időpontú mérés már van!");
+				return;
+			}
+
+			//új mérés hozzáadása
+			measurements.Add(new Measurement(t, s));
+			RefreshList();
+
+			//mezők ürítésa
+			txtTime.Clear();
+			txtSpeed.Clear();
+		}
+	}
 }
