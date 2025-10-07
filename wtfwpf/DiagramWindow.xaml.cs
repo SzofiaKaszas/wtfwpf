9e6220383acf4b2025441829039577a7be06e105
Petrik Diak <diak@petrik.hu>
2025-10-07 13:54:31 +0200

    diagram drawing fully done

--- wtfwpf/DiagramWindow.xaml.cs
+++ wtfwpf/DiagramWindow.xaml.cs
@@ -27,7 +27,72 @@ namespace wtfwpf
 			this.data = data;
 			this.totalDistance = totalDistance;
 
+			Loaded += Betoltes;
+		}
 
+		private void Betoltes(object sender, RoutedEventArgs e)
+		{
+			if(data.Count < 2)
+			{
+				MessageBox.Show("Nincs elég adat a diagramhoz!");
+				return;
+			}
+
+			double margin = 30;
+			double width = canvas.ActualWidth - 2 * margin;
+			double height = canvas.ActualHeight - 2 * margin;
+			double maxTime = data.Max(m => m.Time);
+			double maxSpeed = data.Max(m => m.Speed);
+
+			//első pont kezdőpozíciója
+			Point prev = new Point(margin, margin + height - (data[0].Speed / maxSpeed) * height);
+
+			//minden szomszédos pont összekötése
+			for(int i = 1; i < data.Count; i++)
+			{
+				double x = margin + (data[i].Time / maxTime) * width;
+				double y = margin + height - (data[i].Speed / maxSpeed) * height;
+
+				Line line = new Line()
+				{
+					X1 = prev.X,
+					Y1 = prev.Y,
+					X2 = x,
+					Y2 = y,
+					Stroke = Brushes.SteelBlue,
+					StrokeThickness = 2
+				};
+
+				canvas.Children.Add(line);
+				prev = new Point(x, y);
+			}
+
+			//X tengely
+			Line xAxis = new Line()
+			{
+				X1 = margin,
+				Y1 = margin + height,
+				X2 = margin + width + 5,
+				Y2 = margin + height,
+				Stroke = Brushes.Black,
+				StrokeThickness = 3
+			};
+
+			//Y tengely
+			Line yAxis = new Line()
+			{
+				X1 = margin,
+				Y1 = margin,
+				X2 = margin,
+				Y2 = margin + height + 5,
+				Stroke = Brushes.Black,
+				StrokeThickness = 3
+			};
+
+			canvas.Children.Add(xAxis);
+			canvas.Children.Add(yAxis);
+
+			txtTotal.Text = $"Teljes megtett út: {totalDistance} m";
 		}
 	}
 }
