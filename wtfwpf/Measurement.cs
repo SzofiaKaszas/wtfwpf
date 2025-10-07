30004e3d6e4842bb007fb6b28f2791f16f798fed
Petrik Diak <diak@petrik.hu>
2025-10-07 11:46:34 +0200

    class and visuals done

---
+++ wtfwpf/Measurement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wtfwpf
+{
+	internal class Measurement
+	{
+		public double Time { get; set; }
+		public double Speed { get; set; }
+
+		public Measurement(double time, double speed)
+		{
+			Time = time;
+			Speed = speed;
+		}
+
+		public override string ToString()
+		{
+			return $"{Time} s, {Speed} m/s";
+		}
+
+		public string toFileString()
+		{
+			return $"{Time};{Speed}";
+		}
+	}
+}
